f49997a4352e6dee69ac36bc5a398f7238425939
Sikay <pedrohdezmora@gmail.com>
2020-03-18 20:29:36 +0100

    Creacion del componente SearchForm y llamada y recuperacion de datos de una API de peliculas

---
+++ src/components/SearchForm.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+const API_KEY = "461cd187";
+const BASE_END_POINT = `http://www.omdbapi.com/?apikey=${API_KEY}&`;
+
+export class SearchForm extends Component {
+  state = {
+    inputMovie: ""
+  };
+
+  _handleChange = e => {
+    this.setState({
+      inputMovie: e.target.value
+    });
+  };
+
+  _handleSubmit = e => {
+    e.preventDefault();
+    const { inputMovie } = this.state;
+    fetch(`${BASE_END_POINT}s=${inputMovie}`)
+      .then(response => response.json())
+      .then(results => {
+        console.log(results);
+        const { Search, totalResults } = results;
+      });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this._handleSubmit}>
+        <div className="field has-addons">
+          <div className="control">
+            <input
+              className="input"
+              onChange={this._handleChange}
+              type="text"
+              placeholder="Movie to search..."
+            />
+          </div>
+          <div className="control">
+            <button className="button is-info">Search</button>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
